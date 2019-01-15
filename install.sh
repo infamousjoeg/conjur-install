dd40218a5a22851ba5b8121eb700da68f331b803
Joe Garcia <joe@joe-garcia.com>
2019-01-15 15:55:01 -0500

    more secure set -x commands

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ex pipefail
+set -e pipefail
 
 # This script is meant for quick & easy install via:
 #   $ curl -fsSL https://cybr.rocks/conjur-install | bash -s
@@ -28,9 +28,13 @@ main () {
 update_yumapt () {
     # Check if yum or apt is installed; Update whichever is
     if [ "$(command -v yum)" ]; then
+        set -x
         sudo yum update -y
+        set +x
     elif [ "$(command -v apt)" ]; then
+        set -x
         sudo apt update && sudo apt upgrade -y
+        set +x
     else
         RED='\033[0;31m'
         NC='\033[0m' # No Color
@@ -42,8 +46,10 @@ install_docker () {
     # Check if Docker CE is installed
     if [ -z "$(command -v docker)" ]; then
         # Install Docker CE
+        set -x
         sudo curl -fsSL https://get.docker.com | sh
         sudo usermod -aG docker "${USER}"
+        set +x
     fi
 }
 
@@ -51,20 +57,26 @@ install_dockercompose () {
     # Check if Docker Compose is installed
     if [ -z  "$(command -v docker-compose)" ]; then
         # Install Docker Compose
+        set -x
         sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
         sudo chmod +x /usr/local/bin/docker-compose
+        set +x
     fi
 }
 
 download_conjur () {
     # Download Conjur & pull Docker Images necessary
+    set -x
     sudo curl -o docker-compose.yml https://www.conjur.org/get-started/docker-compose.quickstart.yml
     sudo docker-compose pull
+    set +x
 }
 
 generate_masterkey () {
     # Generate a secure master key for Conjur
+    set -x
     sudo docker-compose run --no-deps --rm conjur data-key generate | sudo tee data_key > /dev/null
+    set +x
     DATA_KEY="$(< data_key)"
     sed -e "s#CONJUR_DATA_KEY:#CONJUR_DATA_KEY: ${DATA_KEY}#" docker-compose.yml > docker-compose-new.yml
     mv -f docker-compose-new.yml docker-compose.yml
@@ -74,7 +86,9 @@ generate_masterkey () {
 
 start_conjur () {
     # Spin up Docker containers for Conjur
+    set -x
     sudo docker-compose up -d
+    set +x
     rm -rf docker-compose.yml
 }
 
@@ -88,12 +102,16 @@ conjur_init () {
     # Initialize Conjur
     API_KEY=$(echo "${CONJUR_INFO}" | awk 'FNR == 11 {print $5}')
     export CONJUR_API_KEY="${API_KEY}"
+    set -x
     sudo docker exec -i "${USER}"_client_1 conjur init -u conjur -a quick-start 
+    set +x
 }
 
 conjur_authn () {
     # Login to Conjur from CLI (Client) container for Admin user
+    set -x
     sudo docker exec -i "${USER}"_client_1 conjur authn login -u admin -p "${CONJUR_API_KEY}"
+    set +x
 }
 
 report_info () {
@@ -103,7 +121,6 @@ report_info () {
     RED='\033[0;31m'
     YELLOW='\033[1;33m'
     NC='\033[0m' # No Color
-    set +x
     echo -e "${GREEN}+++++++++++++++++++++++++++++++++++++++++++++++++++++${NC}"
     echo -e "${RED}SAVE THESE VALUES IN A SAFE PLACE!${NC}"
     echo -e "${GREEN}+++++++++++++++++++++++++++++++++++++++++++++++++++++${NC}"
