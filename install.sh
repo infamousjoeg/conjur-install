3dc31d674caad9ddf8287367d3923081882f01a4
Joe Garcia <joe@joe-garcia.com>
2019-01-15 13:18:47 -0500

    ready for test prior to release

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-set -eoux pipefail
+set -ex pipefail
 
 # This script is meant for quick & easy install via:
-#   $ curl -fsSL https://conjur.cybr.rocks | bash -s
+#   $ curl -fsSL https://cybr.rocks/conjur-install | bash -s
 #
 # NOTE: Make sure to verify the contents of the script
 #       you downloaded matches the contents of install.sh
 #       located at https://github.com/infamousjoeg/conjur-install
 #       before executing.
+#       e.g. $ curl -fsSL https://cybr.rocks/conjur-install -o conjur-install.sh
+#            $ ./conjur-install.sh
 
 main () {
     update_yumapt
@@ -17,8 +19,6 @@ main () {
     generate_masterkey
     start_conjur
     conjur_createacct
-    conjur_init
-    conjur_authn
     report_info
 }
 
@@ -41,9 +41,8 @@ install_docker () {
         # Install Docker CE
         sudo curl -fsSL https://get.docker.com | sh
         sudo usermod -aG docker "${USER}"
-        newgrp docker
     fi
-}   
+}
 
 install_dockercompose () {
     # Check if Docker Compose is installed
@@ -57,26 +56,27 @@ install_dockercompose () {
 download_conjur () {
     # Download Conjur & pull Docker Images necessary
     sudo curl -o docker-compose.yml https://www.conjur.org/get-started/docker-compose.quickstart.yml
-    docker-compose pull
+    sudo docker-compose pull
 }
 
 generate_masterkey () {
     # Generate a secure master key for Conjur
-    docker-compose run --no-deps --rm conjur data-key generate > data_key
+    sudo docker-compose run --no-deps --rm conjur data-key generate > data_key
     DATA_KEY="$(< data_key)"
-    export CONJUR_DATA_KEY="${DATA_KEY}"
+    sed -e "s#CONJUR_DATA_KEY:#CONJUR_DATA_KEY: ${DATA_KEY}#" docker-compose.yml > docker-compose-new.yml
+    mv -f docker-compose-new.yml docker-compose.yml
     rm -rf data_key
 }
 
 start_conjur () {
     # Spin up Docker containers for Conjur
-    docker-compose up -d
+    sudo docker-compose up -d
     rm -rf docker-compose.yml
 }
 
 conjur_createacct () {
     # Configure Conjur & create account
-    CONJUR_INFO=$(docker exec -it conjur-install_conjur_1 conjurctl account create quick-start)
+    CONJUR_INFO=$(sudo docker exec -i "${USER}"_conjur_1 conjurctl account create quick-start)
     export CONJUR_INFO="${CONJUR_INFO}"
 }
 
@@ -84,12 +84,12 @@ conjur_init () {
     # Initialize Conjur
     API_KEY=$(echo "${CONJUR_INFO}" | awk 'FNR == 11 {print $5}')
     export CONJUR_API_KEY="${API_KEY}"
-    docker exec -it conjur-install_client_1 conjur init -u conjur -a quick-start 
+    sudo docker exec -it "${USER}"_client_1 conjur init -u conjur -a quick-start 
 }
 
 conjur_authn () {
     # Login to Conjur from CLI (Client) container for Admin user
-    docker exec -it conjur-install_client_1 conjur authn login -u admin -p "${CONJUR_API_KEY}"
+    sudo docker exec -it "${USER}"_client_1 conjur authn login -u admin -p "${CONJUR_API_KEY}"
 }
 
 report_info () {
@@ -100,10 +100,14 @@ report_info () {
     YELLOW='\033[1;33m'
     NC='\033[0m' # No Color
     echo -e "${GREEN}+++++++++++++++++++++++++++++++++++++++++++++++++++++${NC}"
-    echo -e "${YELLOW}Below is your ${CYAN}Conjur Data Key${YELLOW}, ${CYAN}Conjur Public SSL Certificate${YELLOW}, and ${CYAN}Admin API Key${NC}"
     echo -e "${RED}SAVE THESE VALUES IN A SAFE PLACE!${NC}"
     echo -e "${CYAN}Conjur Data Key:${NC} ${CONJUR_DATA_KEY}"
-    echo "${CONJUR_INFO}"
+    echo -e "${CYAN}Conjur Public SSL Certificate & Admin API Key:${NC}"
+    echo -e "${CONJUR_INFO}"
+    echo -e "${GREEN}+++++++++++++++++++++++++++++++++++++++++++++++++++++${NC}"
+    echo -e "${GREEN}+++++++++++++++++++++++++++++++++++++++++++++++++++++${NC}"
+    echo -e "${YELLOW}Your Conjur environment is running in Docker: ${CYAN}sudo docker ps${NC}"
+    sudo docker ps
     echo -e "${GREEN}+++++++++++++++++++++++++++++++++++++++++++++++++++++${NC}"
 }
 
